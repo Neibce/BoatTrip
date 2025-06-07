664aa60ddf0e1df5e5427b7b72be5f92c27a905f
Jun-Young Yang <y@jun0.dev>
2025-06-07 15:18:56 +0900

    feat: route 표시용 activity 초안 완성

--- app/src/main/java/com/boattrip/boattrip/RouteActivity.kt
+++ app/src/main/java/com/boattrip/boattrip/RouteActivity.kt
@@ -1,20 +1,30 @@
 package com.boattrip.boattrip
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class MainActivity : AppCompatActivity() {
+
+class RouteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_route)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val standardBottomSheet = findViewById<LinearLayout>(R.id.standard_bottom_sheet)
+        val standardBottomSheetBehavior = BottomSheetBehavior.from(standardBottomSheet)
+        standardBottomSheetBehavior.halfExpandedRatio = 0.5f
+        standardBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+
     }
 }
