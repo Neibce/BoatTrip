8389ed964168cd7eb619b422959f309e04a3661c
Jun-Young Yang <y@jun0.dev>
2025-06-09 02:54:09 +0900

    feat: 루트 생성 구현 초안

---
+++ app/src/main/java/com/boattrip/boattrip/llm/LLMService.kt
@@ -0,0 +1,14 @@
+package com.boattrip.boattrip.llm;
+
+import okhttp3.ResponseBody
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST
+
+interface LLMService {
+    @Headers("Content-Type: application/json")
+    @POST("v1/chat/completions")
+    fun getStructuredAnswer(@Header("Authorization") authorization:String, @Body request: LLMRouteRequest): Call<ResponseBody>
+}
